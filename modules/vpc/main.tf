eee3dd3eb34b5a7431e06d7ae1c1492891e3359b
Gokul Mohanraj <thegokulraj@gmail.com>
2026-03-12 12:12:38 +0530

    Adding vpc modules code..

---
+++ modules/vpc/main.tf
@@ -0,0 +1,115 @@
+# Provider
+
+provider "aws" {}
+
+# VPC
+resource "aws_vpc" "this" {
+
+  cidr_block = var.vpc_cidr
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+# Public subnets
+resource "aws_subnet" "public" {
+
+  count = length(var.public_subnets)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-${count.index}"
+  }
+}
+
+# Private subnets
+resource "aws_subnet" "private" {
+
+  count = length(var.private_subnets)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = {
+    Name = "${var.project_name}-private-${count.index}"
+  }
+}
+
+# Elastic IP
+resource "aws_eip" "nat" {
+  count = var.enable_nat_gateway ? 1 : 0
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "nat" {
+
+  count = var.enable_nat_gateway ? 1 : 0
+
+  allocation_id = aws_eip.nat[0].id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.project_name}-nat"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public" {
+
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Public route table associations
+resource "aws_route_table_association" "public" {
+
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat[0].id
+  }
+}
+
+# Private route table associations
+resource "aws_route_table_association" "private" {
+
+  count = length(aws_subnet.private)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
