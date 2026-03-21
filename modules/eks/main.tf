fa2683407eba8af06e441eda6bf06e66ec7b319d
Gokul Mohanraj <thegokulraj@gmail.com>
2026-03-21 10:06:02 +0530

    eks module

---
+++ modules/eks/main.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_role" "eks_cluster_role" {
+
+  name = "${var.cluster_name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_eks_cluster" "this" {
+
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = var.private_subnets
+  }
+}
